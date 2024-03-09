1ab2b6676500f13d3e14c7e2d27230a95c1d903d
Mohammed Dev <mohammeddev@MacBook-Pro-Mohammed.local>
2024-03-09 20:18:15 +0300

    test channels

--- app/Events/MyEvent.php
+++ app/Events/MyEvent.php
@@ -12,22 +12,29 @@ use Illuminate\Queue\SerializesModels;
 
 class MyEvent implements ShouldBroadcast
 {
-  use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 
-  public $data;
+    public $data;
 
-  public function __construct($data)
-  {
-      $this->data = $data;
-  }
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
 
-  public function broadcastOn()
-  {
-      return ['my-channel'];
-  }
+    public function broadcastOn()
+    {
+        // Define an array of channel names you want to broadcast to
+        $channels = [8, 2, 1];
 
-  public function broadcastAs()
-  {
-      return 'my-event';
-  }
+        // Map each channel name to a Channel object
+        return collect($channels)->map(function ($channel) {
+            return new Channel($channel);
+        });
+        //   return ['my-channel'];
+    }
+
+    public function broadcastAs()
+    {
+        return 'my-event';
+    }
 }
